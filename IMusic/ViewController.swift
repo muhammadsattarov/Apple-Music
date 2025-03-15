b876500dff6e10380d7cb466c0c162c1235282e8
Muhammad Sattarov <sattarovmuhammad98@gmail.com>
2025-03-15 22:32:28 +0500

    Initial Commit

---
+++ IMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMusic
+//
+//  Created by user on 15/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ IMusicTests/IMusicTests.swift
@@ -0,0 +1,17 @@
+//
+//  IMusicTests.swift
+//  IMusicTests
+//
+//  Created by user on 15/03/25.
+//
+
+import Testing
+@testable import IMusic
+
+struct IMusicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
